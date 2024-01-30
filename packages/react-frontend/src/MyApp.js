d51e45289974945faf335812dbfa5bd6546e24f6
Nikman800 <76964182+Nikman800@users.noreply.github.com>
2024-01-29 17:14:09 -0800

    Finished step 2 of exercise 3

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -1,13 +1,13 @@
 // src/MyApp.js
 
-import React, { useState } from "react";
 import Table from "./Table";
 import Form from "./Form";
+import React, { useState, useEffect } from 'react';
 
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
-  
+
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
@@ -18,6 +18,18 @@ function MyApp() {
   function updateList(person) {
     setCharacters([...characters, person]);
   }
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => { console.log(error); });
+  }, []);
 
   return (
     <div className="container">
